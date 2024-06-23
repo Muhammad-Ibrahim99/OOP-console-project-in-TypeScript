5a4f249f101754b8b9e0682b0898c7c9f2cf5e94
Muhammad Ibrahim <160318731+Muhammad-Ibrahim99@users.noreply.github.com>
2024-06-23 14:20:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+class Person {
+    _personality;
+    constructor() {
+        this._personality = 'Mystery';
+    }
+    // user input:    
+    userInput(input) {
+        //if else conditionn
+        if (input.includes('Talk to others about yourself opnely')) {
+            this._personality = 'Talk happily';
+        }
+        else if (input.includes('Keep your words to yourself')) {
+            this._personality = 'Talk rude';
+        }
+    }
+    //return function:
+    get personality() {
+        return this._personality;
+    }
+}
+//create Objects:
+class Main {
+    async main() {
+        //await called out
+        const ans = await inquirer.prompt({
+            //Object create
+            type: 'list',
+            name: 'choice',
+            message: chalk.yellow('Tell your behaviour ?'),
+            choices: [
+                '1: talk to others about my self',
+                '2: keep quite dont ask questions'
+            ]
+        });
+        //create a personal class
+        let myPerson = new Person();
+        myPerson.userInput(ans.choice);
+        //output response
+        console.log(`you are ${myPerson.personality}`);
+    }
+}
+const myObject = new Main();
+myObject.main();
